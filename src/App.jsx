0b9f5a184063b8da8b0b8f263d761bf3bf756ace
Ritvik-project <jindalrj2005@gmail.com>
2025-02-28 13:35:42 +0530

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import Dashboard1 from './components/Dashboard1'
-
+// import '@mantine/core/styles.css';
+// import { LeadGrid } from './components/LeadGrid';
+// import { MantineProvider } from '@mantine/core';
 
 
 const App = () => {
